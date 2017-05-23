f91d90bb29e78d068472c1a037448e0f5d41bd9e
kevinlmadison <coolklm121@gmail.com>
2017-05-23 12:35:45 -0400

    update parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -7,11 +7,38 @@
 
 using namespace std;
 
-void parse (vector<string> vs) { //This is where we will look for keywords
-                                 //Need to decide on the command structure
+enum act{
+    inv,
+    go,
+    look,
+    take,
+    drop,
+    use
+};
+
+act hash_it (std::string const& ss) {
+    if(ss == "inventory") return inv;
+    if(ss == "go") return go;
+    if(ss == "look") return look;
+    if(ss == "take") return take;
+    if(ss == "drop") return drop;
+    if(ss == "use") return use;
+}
+
+void parse (vector<string> vs) { 
+    //This is where we will look for keywords
+    ////Need to decide on the command structure
+    switch (hash_it(vs[0])) {
+        case inv:
+            cout << "Print inventory" << endl;
+        default:
+            cout << "I don't understand" << endl;
+    }
+/*
     for (auto& i : vs) {
         cout << i << endl;
     }
+*/
 }
 
 vector<string> tokenize (string s) {
@@ -31,9 +58,11 @@ bool u_input () {
     cout << "Tell me what to do. type \'quit\' or \'q\'to quit " << endl;
     getline(cin, com);
     tk = tokenize(com);
-    if(tk[0] == "quit" || tk[0] == "q") { //this is how you quit the game
+    if(tk[0] == "quit" || tk[0] == "q") { 
         return true;
     }
+    //This is how you quit the game
+    //This is pretty ugly, will revisit after sleep
 
     //Need to figure a way to save the game state.
     parse(tk);
